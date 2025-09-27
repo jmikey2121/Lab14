df3459a49e802eae79f30ba71d61b3ddcbbf0b61
Justin <jmikey21@gmail.com>
2025-09-27 09:13:32 -0700

    final commit

--- Lab14.cpp
+++ Lab14.cpp
@@ -31,3 +31,33 @@ public:
         cout << endl;
     }
 };
+int main() {
+    cout << "Color Values" << endl << endl;
+
+    // Yellow
+    Color yellow;
+    yellow.setRed(255);
+    yellow.setGreen(255);
+    yellow.setBlue(0);
+    cout << "Yellow:" << endl;
+    yellow.print();
+
+    // Peach
+    Color peach;
+    peach.setRed(255);
+    peach.setGreen(218);
+    peach.setBlue(185);
+    cout << "Peach:" << endl;
+    peach.print();
+
+    // Gold
+    Color gold;
+    gold.setRed(255);
+    gold.setGreen(215);
+    gold.setBlue(0);
+    cout << "Gold:" << endl;
+    gold.print();
+
+    return 0;
+}
+
