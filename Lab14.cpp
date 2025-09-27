4dd37c0c15085e17c31288848d960479ce6b9e75
Justin <jmikey21@gmail.com>
2025-09-27 08:50:13 -0700

    commit

---
+++ Lab14.cpp
@@ -0,0 +1,12 @@
+// COMSC-210 | Lab 13 | Justin Mikesell
+// IDE used: Visual Studio Code 
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
